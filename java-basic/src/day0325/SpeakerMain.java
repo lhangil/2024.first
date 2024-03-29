c6ea961cadff38ac85213b6a932483efcfcbb90b
DESKTOP-O3LMC10\User <NBGR1031@gmail.com>
2024-03-29 11:13:43 +0900

    0329

---
+++ java-basic/src/day0325/SpeakerMain.java
@@ -0,0 +1,18 @@
+package day0325;
+
+public class SpeakerMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Speaker speaker = new Speaker(90);
+		speaker.showVolume();
+		
+		speaker.volumeUp();
+		speaker.showVolume();
+		
+		speaker.volumeUp();
+		speaker.showVolume();
+
+	}
+
+}
